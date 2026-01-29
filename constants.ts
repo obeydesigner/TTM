5cc25e210532fe430cf9a64da342e1463139e7e3
Junsu Jeong <junsujeong@Junsus-MacBook-Pro.local>
2026-01-29 11:33:40 +0900

    first upload

---
+++ constants.ts
@@ -0,0 +1,2 @@
+// All application code has been moved into index.html.
+// This file is no longer needed.
---
+++ index.tsx
@@ -0,0 +1,2 @@
+// All application code has been moved into index.html to allow it to run directly from the filesystem.
+// This file is no longer needed.
